64105a85b1744d2c591468418e46257a2afca887
yunanjeong <njhgle@gmail.com>
2022-11-16 19:44:46 +0900

    불필요 모듈변수 삭제

--- modules/ec2/multi_ubuntu/variables.tf
+++ modules/ec2/multi_ubuntu/variables.tf
@@ -2,22 +2,21 @@
 # Set Up Basic
 ######################################################################
 variable "node_count"   {default = 1}
-variable "ami"          {default = "ami-063454de5fe8eba79" } # "Canonical, Ubuntu, 22.04 LTS, amd64 jammy image build on 2022-04-20"
-variable "instance_type"{default = "t2.micro"  }
-variable "tag_name"     {default = "ubuntu-by-terraform"}
+variable "ami"          {default = "ami-063454de5fe8eba79"} # "Canonical, Ubuntu, 22.04 LTS, amd64 jammy image build on 2022-04-20"
+variable "instance_type"{default = "t2.micro"}
 variable "tags"{
-  description = "instance tags"
   type = map(string)
   default = ({
-    Owner = "xxxxx@gmail.com"
+    Name    = "yunan-ubuntu-terraform"
+    Owner   = "xxxxx@gmail.com"
     Service = "tag-Service"
   })
 }
 ######################################################################
 # Key Pair for Secure Connection
 ######################################################################
-variable "key_name"{  default = "my_keypair_name"  }
-variable "private_key_path"{  default = "/home/ubuntu/.ssh/my_keypair_name.pem"  }
+variable "key_name"        {default = "my_keypair_name"}
+variable "private_key_path"{default = "/home/ubuntu/.ssh/my_keypair_name.pem"}
 variable "ssh_cidr_blocks"{
   description = "인스턴스에서 접속을 허가해줄 로컬PC의 public ip 목록"
   type = list(string)
