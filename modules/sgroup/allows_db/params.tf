742e0422809cfe8242f449b6c9efce706127558e
yunanjeong <njhgle@gmail.com>
2022-11-17 20:16:53 +0900

    주석추가

--- modules/sgroup/allows_db/params.tf
+++ modules/sgroup/allows_db/params.tf
@@ -1,8 +1,13 @@
+######################################################################
+# Module Input Params (variables.tf)
+######################################################################
 variable "sgroup_name"  {default = "allows_db"}
 variable "allowed_list" {default = ["0.0.0.0/32"]}
 variable "db_port"      {default = 1433} # sql server default port
 
-
+######################################################################
+# Module Output Params (output.tf)
+######################################################################
 output "id" {
   description = "ID of the EC2 instance"
   value       = aws_security_group.allows.id
